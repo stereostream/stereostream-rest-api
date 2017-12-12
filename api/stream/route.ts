dc2cc4f0fddea412f304f4235830b6c37f456aae
Samuel Marks <samuelmarks@gmail.com>
2017-12-13 03:47:47 +1100

    Error handling for stream route; upgraded dependencies

--- api/stream/route.ts
+++ api/stream/route.ts
@@ -5,21 +5,25 @@ import * as restify from 'restify';
 
 import { IOrmReq } from 'orm-mw';
 import * as mime from 'mime-types';
+import { fmtError, NotFoundError } from 'custom-restify-errors';
 
 export const read = (app: restify.Server, namespace: string = ''): void => {
     app.get(`${namespace}/:name`,
         (req: restify.Request & IOrmReq, res: restify.Response, next: restify.Next) => {
+            const sn = `${namespace}/:name 
+                with ${process.env.STREREOSTREAM_BROADAST}${path.sep}${req.params.name}`;
+            if (process.env.STREREOSTREAM_BROADAST == null || req.params.name == null)
+                return next(new NotFoundError(sn));
+
             const filePath = path.join(process.env.STREREOSTREAM_BROADAST, req.params.name);
             const ext = path.extname(filePath);
 
             fs.exists(filePath, exists => {
                 if (!exists) {
-                    res.send(404);
+                    return next(new NotFoundError(sn));
                 } else fs.readFile(filePath, (err, contents) => {
-                    if (err || !contents) { // Error or empty playlist
-                        res.writeHead(500);
-                        res.end();
-                    }
+                    if (err != null) return next(fmtError(err));
+                    else if (!contents) return next(new NotFoundError(sn));
                     else {
                         res.setHeader('Content-Type', mime.lookup(ext) || 'application/octet-stream');
                         res.writeHead(200);
@@ -36,10 +40,9 @@ export const read = (app: restify.Server, namespace: string = ''): void => {
                             })
                         else */
                         res.end(contents, 'utf-8');
+                        return next();
                     }
                 });
-
-                return next();
             })
         }
     );
